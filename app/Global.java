6cd353d0f50ba9a5389224bd718472588a4f6c9a
mattia <marwa.atya@gmail.com>
2015-05-17 17:26:27 +0200

    Remove deprecated constructor

--- app/Global.java
+++ app/Global.java
@@ -9,21 +9,21 @@ import com.mongodb.MongoClient;
 
 import controllers.MorphiaObject;
 
-
 public class Global extends GlobalSettings {
 	@Override
 	public void onStart(play.Application arg0) {
 		super.beforeStart(arg0);
-		Logger.debug("** onStart **"); 
+		Logger.debug("** onStart **");
 		try {
-			MorphiaObject.mongo =new  MongoClient("127.0.0.1", 27017);
+			MorphiaObject.mongo = new MongoClient("127.0.0.1", 27017);
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 		MorphiaObject.morphia = new Morphia();
-		MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(MorphiaObject.mongo, "test");
-		MorphiaObject.datastore.ensureIndexes();   
-		MorphiaObject.datastore.ensureCaps();  
+		MorphiaObject.datastore = MorphiaObject.morphia.createDatastore(
+				MorphiaObject.mongo, "test");
+		MorphiaObject.datastore.ensureIndexes();
+		MorphiaObject.datastore.ensureCaps();
 
 		Logger.debug("** Morphia datastore: " + MorphiaObject.datastore.getDB());
 	}
