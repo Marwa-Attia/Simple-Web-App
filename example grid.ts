13d504149fe3a6a5453d3795884fb3f620539e8b
Marwa Attia <marwa.atya@gmail.com>
2017-01-27 15:49:02 -0600

    Create example grid.ts

---
+++ example grid.ts
@@ -0,0 +1,30 @@
+<wf-grid [dataset]="dataset" [enablePaging]="true">
+    <column [value]="'id'" [header]="'Id'"></column>
+    <column [value]="'city'" [header]="'City'"></column>
+    <column [value]="'country'" [header]="'Country'"></column>
+</wf-grid>
+
+
+dataset: any[]=[{id:"A",city:"Cairo",country:"Egypt"},
+{id:"B",city:"Cairo",country:"Egypt"},
+{id:"C",city:"Cairo",country:"Egypt"},
+{id:"D",city:"Cairo",country:"Egypt"},
+{id:"E",city:"Cairo",country:"Egypt"},
+{id:"F",city:"Cairo",country:"Egypt"},
+{id:"G",city:"Cairo",country:"Egypt"},
+{id:"H",city:"Cairo",country:"Egypt"},
+{id:"I",city:"Cairo",country:"Egypt"},
+{id:"J",city:"Cairo",country:"Egypt"},
+{id:"K",city:"Cairo",country:"Egypt"},
+{id:"L",city:"Cairo",country:"Egypt"},
+{id:"M",city:"Cairo",country:"Egypt"},
+{id:"N",city:"Cairo",country:"Egypt"},
+{id:"O",city:"Cairo",country:"Egypt"},
+{id:"P",city:"Cairo",country:"Egypt"},
+{id:"Q",city:"Cairo",country:"Egypt"},
+{id:"R",city:"Cairo",country:"Egypt"},
+{id:"S",city:"Cairo",country:"Egypt"},
+{id:"T",city:"Cairo",country:"Egypt"},
+{id:"U",city:"Cairo",country:"Egypt"},
+{id:"V",city:"Cairo",country:"Egypt"},
+{id:"W",city:"Cairo",country:"Egypt"}];
